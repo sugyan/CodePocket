9a3cc5f8379e3e1530541570bf0df819ca309fad
sugyan <sugi1982@gmail.com>
2009-05-11 01:31:07 +0900

    python/hatebu

---
+++ lang/python/hatebu/hatebu.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import codecs
+import feedparser
+import sys
+from xmlrpclib import ServerProxy
+
+sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
+
+try:
+    # アカウント名取得
+    user = sys.argv[1]
+    
+    # 新着ブックマーク
+    rss = 'http://b.hatena.ne.jp/bookmarklist.rss?url=http%3A%2F%2Fd.hatena.ne.jp%2F' + user
+    feed = feedparser.parse(rss)
+    for entry in reversed(feed['entries']):
+        # 記事のタイトルは entry['title'] で取得
+        date = "%4d/%02d/%02d" % entry['updated_parsed'][:3]
+        print '%s %s: %s' % (date, entry['author'], entry['summary'])
+
+    # 被ブックマーク総数
+    server = ServerProxy('http://b.hatena.ne.jp/xmlrpc')
+    total = server.bookmark.getTotalCount('http://d.hatena.ne.jp/' + user)
+    print 'total: %d' % (total)
+        
+except IndexError:
+    print 'usage: ./hatebu.py <your_hatena_id>'
